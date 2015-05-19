5c08ceb1ed6a4c7af4f186ad83d4d9023235426c
YIFAN YANG <YANG@d-172-27-98-24.bootp.Virginia.EDU>
2015-05-18 22:56:47 -0400

    initial

---
+++ Temp.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def ILOVEWHO( name):
+    print 'I Love ' + name
+
+if __name__ == '__main__':
+    ILOVEWHO('qingyun')
